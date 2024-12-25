9bb607aeb1b616d2f03fe2b58acf898c355a5daa
Raphael <raphael.pb.rialland@gmail.com>
2024-12-25 17:19:28 +0100

    finished unifying preprocessing, now saves all 4 data files to data for training

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,7 @@ import sys; sys.path.append(os.getenv('MP'))
 import nn
 
 def load_data(path):
-  with open(path, 'r') as f:
+  with open(path, 'r', encoding='utf-8') as f:
     data = f.read()
   return data
 
@@ -67,7 +67,7 @@ def one_hot_encode(x, n_classes=None, dtype='int'):
     n_classes = np.max(x) # max class number found in array
   return np.eye(n_classes, dtype=dtype)[x]
 
-def train_split(data, ratio, n_classes):
+def train_split(data, ratio):
     # Split x, y
     x, y = data[:,:-1], data[:,-1]
 
@@ -84,9 +84,11 @@ def train_split(data, ratio, n_classes):
 def main():
   batch_size = 4
   vocab_size = 300
-  data_path = r"data/pico_stories.csv"
+  sep = '<|endoftext|>'
+  data_path = r"data/pico_stories.txt"
 
   data = load_data(data_path)
+  data = data.split(sep)
 
   # Initialise tokeniser and vectoriser
   tokenizer = nn.text.BytePairTokenizer(vocab_size)
@@ -105,7 +107,8 @@ def main():
 
   # Split and encode
   x_train, x_val, y_train, y_val = train_split(data, 0.2)
-  y_train, y_val = one_hot_encode(y_train, n_classes=vocab_size), one_hot_encode(y_val, n_classes=vocab_size)
+  y_train = one_hot_encode(y_train, n_classes=vocab_size)
+  y_val = one_hot_encode(y_val, n_classes=vocab_size)
 
   # Batch data
   from nn.data import batch
@@ -115,10 +118,10 @@ def main():
   y_val = batch(y_val, batch_size)
 
   # Save data
-  np.save(x_train, r"data/x_train.npy")
-  np.save(y_train, r"data/y_train.npy")
-  np.save(x_val, r"data/x_val.npy")
-  np.save(y_val, r"data/y_val.npy")
+  np.save(r"data/x_train.npy", x_train)
+  np.save(r"data/y_train.npy", y_train)
+  np.save(r"data/x_val.npy", x_val)
+  np.save(r"data/y_val.npy", y_val)
 
 if __name__ == '__main__':
   main()
