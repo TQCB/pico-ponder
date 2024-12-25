02a2b27dc70c50e5011da6657e689b52094edffe
Raphael <raphael.pb.rialland@gmail.com>
2024-12-25 17:11:00 +0100

    changed return to txt file and fixed indent

--- pico_stories_generator.py
+++ pico_stories_generator.py
@@ -1,16 +1,14 @@
 def main():
   import csv
 
-  with open(r"C:\Users\rapha\Documents\datasets\tiny_stories\TinyStoriesV2-GPT4-valid.txt") as f:
+  with open(r"C:\Users\rapha\Documents\datasets\tiny_stories\TinyStoriesV2-GPT4-valid.txt", 'r', encoding='utf-8') as f:
     str = f.read()
 
   reduced_str = str[:int(len(str) / 22)] # divide by 22 to get roughly 1mb text
   reduced_str = reduced_str.replace('\n', '')
-  sentences = reduced_str.split('<|endoftext|>')
 
-  with open(r"C:\Users\rapha\Documents\datasets\tiny_stories\pico_stories.csv", 'w') as f:
-    wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-    wr.writerow(sentences)
+  with open(r"data/pico_stories.txt", 'w', encoding='utf-8') as f:
+    f.write(reduced_str)
 
-  if __name__ == '__main__':
+if __name__ == '__main__':
     main()
