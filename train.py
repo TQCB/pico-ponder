723c4cf0b1a238c7eba95209033a8c387b7b96d1
Raphael <raphael.pb.rialland@gmail.com>
2025-01-07 19:07:55 +0100

    updated model params and added swish meta layer

--- train.py
+++ train.py
@@ -13,39 +13,54 @@ def main():
 
   # Model config
   # Embed size must be able to be split across heads
-  output_size = 300
-  embed_size = 16
-  seq_len = 64
-  n_heads = 4
-  n_transformers = 3
+  output_size = 32
+  embed_size = 8
+  seq_len = 1
+  n_heads = 2
+  n_transformers = 2
 
   # Model definition
   model = nn.network.Network()
 
-  transformer = l.MetaLayer([
-    l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads),
-    l.LayerNormalisation(embed_size),
+  swish_ffn = l.MetaLayer([
     l.Dense2D(input_dim=embed_size, output_dim=embed_size),
     l.Activation(nn.activations.Swish),
-    l.LayerNormalisation(embed_size)
-  ])
+    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
+    l.Activation(nn.activations.Swish),
+  ], clip=1)
+
+  transformer = l.MetaLayer([
+    l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads, DEBUG_CLIPPING=True),
+    l.LayerNormalisation(embed_size),
+    swish_ffn,
+    l.LayerNormalisation(embed_size),
+  ], clip=1)
 
   model.add(l.PositionalEmbedding(seq_len=seq_len, output_dim=embed_size, vocab_size=output_size))
   for i in range(n_transformers):
     model.add(transformer)
-  model.add(l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads, return_sequences=False))
+  model.add(l.LayerNormalisation(embed_size))
+  model.add(l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads, return_sequences=False, DEBUG_CLIPPING=True))
+  model.add(l.LayerNormalisation(embed_size))
   model.add(l.Dense1D(input_dim=embed_size, output_dim=output_size))
   model.add(l.Activation(nn.activations.Softmax))
+ 
+  lr = nn.optimizers.LearningRateSchedule(1e-3)
+  # lr = nn.optimizers.ExponentialDecaySchedule(initial_lr=1e-1, decay_steps=60, decay_rate=0.5, min=1e-10)
+  save_cb =  nn.callbacks.SaveOnProgressCallback(r'checkpoints')
 
-  lr = nn.optimizers.LinearCycleSchedule(0.001, 0.1, 1000)
-  save_cb =  nn.callbacks.SaveOnProgressCallback(r'data/model_checkpoints\first_run')
+  print(x_train.shape)
 
   model.build(nn.losses.cce, nn.losses.d_cce, nn.metrics.categorical_accuracy, lr)
   model.fit(x_train, y_train,
-            x_val=x_val, y_val=y_val,
-            epochs=3,
-            callbacks=[save_cb],
-            batch_print_steps=50)
+            # x_val=x_val, y_val=y_val,
+            validation=False,
+            epochs=100,
+            # callbacks=[save_cb],
+            batch_print_steps=100)
+
+  with open('checkpoints/history.txt', 'w') as f:
+    f.write(str(model.history))
 
 if __name__ == '__main__':
   main()
