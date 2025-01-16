5a55a70b91b841582c0185f5b80a1b178c7614f6
Raphaël Rialland <raphael.pb.rialland@gmail.com>
2025-01-16 23:58:55 +0100

    updated to use new library name

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv; load_dotenv()
 import sys, os; sys.path.append(os.getenv('MP'))
 
-import nn
-import nn.layers as l
+import femto_flow as ff
+import femto_flow.layers as l
 import numpy as np
 
 def main():
@@ -15,70 +15,57 @@ def main():
 
   # Model config
   # Embed size must be able to be split across heads
-  output_size = 64
+  output_size = 256
   embed_size = 64
-  seq_len = 4
+  seq_len = 2
   n_heads = 8
   n_transformers = 2
 
   # Model definition
-  model = nn.network.Network()
+  model = ff.network.Network()
 
-  # THESE DON'T WORK BECAUSE THEY SHARE WEIGHTS!!!!!
-  # Need some sort of factory to generate these independently
-
-  # ffn = l.MetaLayer([
-  #   l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-  #   l.Activation(nn.activations.Swish),
-  #   l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-  #   l.Activation(nn.activations.Swish),
-  # ], clip=1)
-
-  # transformer = l.MetaLayer([
-  #   l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads),
-  #   l.LayerNormalisation(embed_size),
-  #   l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-  #   l.Activation(nn.activations.Swish),
-  #   l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-  #   l.Activation(nn.activations.Swish),
-  #   l.LayerNormalisation(embed_size),
-  # ], clip=1)
+  def transformer_factory():
+    return l.MetaLayer([
+    l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads),
+    l.LayerNormalisation(embed_size),
+    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
+    l.Activation(ff.activations.Swish),
+    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
+    l.InvertedDropout(0.2),
+    l.Activation(ff.activations.Swish),
+    l.LayerNormalisation(embed_size),
+    ])
 
   model.add(l.PositionalEmbedding(seq_len=seq_len, output_dim=embed_size, vocab_size=output_size))
-  
-  # transformer
-  model.add(l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads))
-  model.add(l.LayerNormalisation(embed_size))
-  model.add(l.Dense2D(input_dim=embed_size, output_dim=embed_size))
-  model.add(l.Activation(nn.activations.Swish))
-  model.add(l.Dense2D(input_dim=embed_size, output_dim=embed_size))
-  model.add(l.Activation(nn.activations.Swish))
-  model.add(l.LayerNormalisation(embed_size))
-
+  for _ in range(n_transformers):
+    model.add(transformer_factory())
   model.add(l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads, return_sequences=False))
   model.add(l.LayerNormalisation(embed_size))
   model.add(l.Dense1D(input_dim=embed_size, output_dim=output_size))
-  model.add(l.Activation(nn.activations.Softmax))
- 
-  lr = nn.optimizers.LearningRateSchedule(1e-5)
-  opt = nn.optimizers.AdamOptimizer
-  # lr = nn.optimizers.ExponentialDecaySchedule(initial_lr=1e-1, decay_steps=100, decay_rate=0.96, min=1e-3)
-  save_cb =  nn.callbacks.SaveOnProgressCallback(r'checkpoints')
 
-  model.build(nn.losses.cce, nn.losses.d_cce,
-              nn.metrics.categorical_accuracy,
+  model.add(l.Activation(ff.activations.Softmax))
+ 
+  lr = ff.optimizers.LearningRateSchedule(1e-3)
+  lr = ff.optimizers.ExponentialDecaySchedule(1e-2, 100, 0.8, min=1e-4)
+  opt = ff.optimizers.AdamOptimizer
+
+  model.build(ff.losses.cce, ff.losses.d_cce,
+              ff.metrics.categorical_accuracy,
               optimizer=opt, 
               learning_rate_schedule=lr,)
 
+  print(f"Batches/Epoch: {x_train.shape[0]}")
   print(f"Parameter count: {model.param_count}")
+  
+  lr_cb = ff.callbacks.PrintLRCallback()
+  save_cb =  ff.callbacks.SaveOnProgressCallback(r'checkpoints')
 
   model.fit(x_train, y_train,
-            # x_val=x_val, y_val=y_val,
-            validation=False,
-            epochs=100,
+            x_val=x_val, y_val=y_val,
+            validation=True,
+            epochs=30,
             # callbacks=[lr_cb],
-            batch_print_steps=100,
-            clip=10)
+            batch_print_steps=20)
 
   with open('checkpoints/history.txt', 'w') as f:
     f.write(str(model.history))
