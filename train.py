36403ba6b8c989fcb73929a08e62ee435aee5fe4
Raphaël Rialland <raphael.pb.rialland@gmail.com>
2025-01-16 01:11:21 +0100

    added transformer factory to make model definition easier

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ def main():
 
   # Model config
   # Embed size must be able to be split across heads
-  output_size = 64
-  embed_size = 64
+  output_size = 40
+  embed_size = 32
   seq_len = 4
   n_heads = 8
   n_transformers = 2
@@ -24,48 +24,45 @@ def main():
   # Model definition
   model = nn.network.Network()
 
-  ffn = l.MetaLayer([
-    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-    l.Activation(nn.activations.Swish),
-    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-    l.Activation(nn.activations.Swish),
-  ], clip=1)
-
-  transformer = l.MetaLayer([
+  def transformer_factory():
+    return l.MetaLayer([
     l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads),
     l.LayerNormalisation(embed_size),
-    ffn,
+    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
+    l.Activation(nn.activations.Swish),
+    l.Dense2D(input_dim=embed_size, output_dim=embed_size),
+    l.Activation(nn.activations.Swish),
     l.LayerNormalisation(embed_size),
-  ], clip=1)
+    ])
 
   model.add(l.PositionalEmbedding(seq_len=seq_len, output_dim=embed_size, vocab_size=output_size))
-  for i in range(n_transformers):
-    model.add(transformer)
-  model.add(l.LayerNormalisation(embed_size))
+  for _ in range(n_transformers):
+    model.add(transformer_factory())
   model.add(l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads, return_sequences=False))
   model.add(l.LayerNormalisation(embed_size))
   model.add(l.Dense1D(input_dim=embed_size, output_dim=output_size))
   model.add(l.Activation(nn.activations.Softmax))
  
-  lr = nn.optimizers.LearningRateSchedule(1e-5)
+  lr = nn.optimizers.LearningRateSchedule(1e-3)
   opt = nn.optimizers.AdamOptimizer
-  # lr = nn.optimizers.ExponentialDecaySchedule(initial_lr=1e-1, decay_steps=100, decay_rate=0.96, min=1e-3)
-  save_cb =  nn.callbacks.SaveOnProgressCallback(r'checkpoints')
 
   model.build(nn.losses.cce, nn.losses.d_cce,
               nn.metrics.categorical_accuracy,
               optimizer=opt, 
               learning_rate_schedule=lr,)
 
+  print(f"Batches/Epoch: {x_train.shape[0]}")
   print(f"Parameter count: {model.param_count}")
+  
+  lr_cb = nn.callbacks.PrintLRCallback()
+  save_cb =  nn.callbacks.SaveOnProgressCallback(r'checkpoints')
 
   model.fit(x_train, y_train,
-            # x_val=x_val, y_val=y_val,
+            x_val=x_val, y_val=y_val,
             validation=False,
-            epochs=100,
+            epochs=99_999,
             # callbacks=[lr_cb],
-            batch_print_steps=100,
-            clip=10)
+            batch_print_steps=100)
 
   with open('checkpoints/history.txt', 'w') as f:
     f.write(str(model.history))
