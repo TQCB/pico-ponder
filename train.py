740901f3c59c6dc321469fa230a58d4d8a232bd7
Raphael <raphael.pb.rialland@gmail.com>
2025-01-27 17:53:32 +0100

    added head count error check and generally cleaned up the code

--- train.py
+++ train.py
@@ -14,58 +14,77 @@ def main():
   ##### MODEL DEFINITION #####
 
   # Model config
-  # Embed size must be able to be split across heads
-  output_size = 256
+  output_size = 64
   embed_size = 64
-  seq_len = 2
-  n_heads = 8
-  n_transformers = 2
+  seq_len = 16
+  n_heads = 4
+  n_transformers = 8
+  dropout_rate = 0.0
+  
+  # Embed size must be able to be split across heads
+  assert embed_size % n_heads == 0, "Embed size cannot be split across attention heads."
+  
+  # Training config
+  epochs = 20
+
+  decay = False
+  max_lr = 1e-3
+  min_lr = 1e-5
+  decay_rate = (min_lr/max_lr) ** (1/epochs) # decay rate to achieve min_lr from max_lr over n epochs
 
   # Model definition
   model = ff.network.Network()
 
-  def transformer_factory():
+  def create_transformer():
     return l.MetaLayer([
     l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads),
     l.LayerNormalisation(embed_size),
     l.Dense2D(input_dim=embed_size, output_dim=embed_size),
+    l.InvertedDropout(dropout_rate),
     l.Activation(ff.activations.Swish),
     l.Dense2D(input_dim=embed_size, output_dim=embed_size),
-    l.InvertedDropout(0.2),
+    l.InvertedDropout(dropout_rate),
     l.Activation(ff.activations.Swish),
     l.LayerNormalisation(embed_size),
     ])
 
   model.add(l.PositionalEmbedding(seq_len=seq_len, output_dim=embed_size, vocab_size=output_size))
   for _ in range(n_transformers):
-    model.add(transformer_factory())
+    model.add(create_transformer())
   model.add(l.MultiHeadSelfAttention(input_dim=embed_size, n_dim=embed_size, n_heads=n_heads, return_sequences=False))
   model.add(l.LayerNormalisation(embed_size))
   model.add(l.Dense1D(input_dim=embed_size, output_dim=output_size))
-
   model.add(l.Activation(ff.activations.Softmax))
- 
-  lr = ff.optimizers.LearningRateSchedule(1e-3)
-  lr = ff.optimizers.ExponentialDecaySchedule(1e-2, 100, 0.8, min=1e-4)
-  opt = ff.optimizers.AdamOptimizer
 
+  if decay:
+    lr = ff.optimizers.ExponentialDecaySchedule(initial_lr=max_lr,
+                                                decay_steps=x_train.shape[0],
+                                                decay_rate=decay_rate,
+                                                min=min_lr)
+    print(f"Epoch LR decay rate: {decay_rate:.3f}")
+
+  else:
+    lr = ff.optimizers.LearningRateSchedule(max_lr)
+
+  opt = ff.optimizers.AdamOptimizer
+  
   model.build(ff.losses.cce, ff.losses.d_cce,
               ff.metrics.categorical_accuracy,
               optimizer=opt, 
               learning_rate_schedule=lr,)
 
-  print(f"Batches/Epoch: {x_train.shape[0]}")
-  print(f"Parameter count: {model.param_count}")
+  print(f"Batches per epoch: {x_train.shape[0]}")
+  print(f"Parameter count:   {model.param_count:,.0f}")
   
   lr_cb = ff.callbacks.PrintLRCallback()
-  save_cb =  ff.callbacks.SaveOnProgressCallback(r'checkpoints')
+  save_cb =  ff.callbacks.SaveOnProgressCallback(r'checkpoints/char_gen')
 
   model.fit(x_train, y_train,
             x_val=x_val, y_val=y_val,
             validation=True,
-            epochs=30,
-            # callbacks=[lr_cb],
-            batch_print_steps=20)
+            epochs=epochs,
+            callbacks=[save_cb],
+            batch_print_steps=10)
 
   with open('checkpoints/history.txt', 'w') as f:
     f.write(str(model.history))
